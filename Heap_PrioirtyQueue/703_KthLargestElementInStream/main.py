8da100d18711335cea061eec2f34190d190ac070
VincentChen0110 <59216100+vincent-0110@users.noreply.github.com>
2022-06-18 16:07:17 +0800

    add 703

---
+++ Heap_PrioirtyQueue/703_KthLargestElementInStream/main.py
@@ -0,0 +1,32 @@
+import heapq
+class KthLargest(object):
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        self.pool = nums
+        heapq.heapify(self.pool)
+        self.k = k
+        while len(self.pool) > k:
+            heapq.heappop(self.pool)
+            print(self.pool)
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        if len(self.pool) < self.k:
+            heapq.heappush(self.pool, val)
+        elif val > self.pool[0]:
+            heapq.heapreplace(self.pool, val)
+        return self.pool[0]
+
+heap = KthLargest(3, [4, 5, 8, 2])
+heap.add(3);   
+heap.add(5);   
+heap.add(10);  
+heap.add(9);   
+heap.add(4);   
