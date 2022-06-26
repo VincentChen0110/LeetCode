8a248a3d5c2c50902b896e5d3613fc0444aa9af5
VincentChen0110 <vincentfosterchen@gmail.com>
2022-06-26 15:10:45 +0800

    add 2

--- Graphs/207_CourseSchedule/bfs.py
+++ Graphs/207_CourseSchedule/bfs.py
@@ -1,3 +1,6 @@
+# Explaination :
+# https://leetcode.com/problems/course-schedule/discuss/368508/Python3-Breadth-first-search-for-cycle-detection
+# Time Complexity O(V + E) Linear running time
 from collections import deque, defaultdict
 def canFinish(numCourses, prerequisites):
     todo = {i: set() for i in range(numCourses)} 
@@ -5,14 +8,14 @@ def canFinish(numCourses, prerequisites):
     for i,j in prerequisites:
         todo[i].add(j)
         graph[j].add(i)
-    print(todo)
-    print(graph)
     q = deque([])
     for k,v in todo.items():
         if len(v) == 0:
             q.append(k)
+    print(q)
     while q:
         n = q.popleft()
+        print(n, "pop here")
         for i in graph[n]:
             todo[i].remove(n)
             if len(todo[i]) == 0:
@@ -20,6 +23,6 @@ def canFinish(numCourses, prerequisites):
         todo.pop(n)
     return not todo
 
-numCourses = 2
-prerequisites = [[1,0],[0,1]]
+numCourses = 6
+prerequisites = [[2,1],[4,1],[3,2],[5,4],[2,3]]
 print(canFinish(numCourses, prerequisites))
