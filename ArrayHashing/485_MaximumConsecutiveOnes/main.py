646ddd47c70d3005f2d9309a5c9d7cf6fcc44dae
VincentChen0110 <vincentfosterchen@gmail.com>
2022-07-06 15:12:10 +0800

    add 485

---
+++ ArrayHashing/485_MaximumConsecutiveOnes/main.py
@@ -0,0 +1,13 @@
+def findMaxConsecutiveOnes(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    ans, final_ans = 0, 0
+    for num in nums:
+        if num==1:
+            ans+=1
+        else:
+            ans=0
+        if final_ans < ans: final_ans = ans
+    return final_ans
