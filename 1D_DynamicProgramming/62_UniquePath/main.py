9e9b4bcd4df2dd41a5bbe30f98503a292ff8d70b
VincentChen0110 <vincentfosterchen@gmail.com>
2022-08-23 11:05:57 -0500

    add 52

---
+++ 1D_DynamicProgramming/62_UniquePath/main.py
@@ -0,0 +1,16 @@
+def uniquePaths(self, m, n):
+    """
+    :type m: int
+    :type n: int
+    :rtype: int
+    """
+    dp = [[0]*n for _ in range(m)]
+    for i in range(n):
+        dp[0][i] = 1
+    for i in range(m):
+        dp[i][0] = 1
+        
+    for i in range(1,m):
+        for j in range(1,n):
+            dp[i][j] = dp[i-1][j]+dp[i][j-1]
+    return dp[m-1][n-1]
